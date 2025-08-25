77abbd651f377895940e42f105dbdefc8d67f47c
sarthika321 <psarthika1@gmail.com>
2025-08-25 11:20:32 +0530

    binary search tree lowest common ancestor is uploaded

---
+++ Hackerrank/week4/Binary Search Tree Lowest Common Ancestor.c
@@ -0,0 +1,26 @@
+
+
+/* you only have to complete the function given below.  
+node is defined as  
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+*/
+struct node *lca( struct node *root, int v1, int v2 ) {
+    if(root==NULL) return NULL;
+    
+    if(v1<root->data && v2<root->data){
+        return lca(root->left,v1,v2);
+    } else if(v1>root->data && v2>root->data){
+        return lca(root->right,v1,v2);
+    }else{
+        return root;
+    }
+}
+
