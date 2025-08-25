ff8be0f5442b9c4391614fb012ca841552c553e5
sarthika321 <psarthika1@gmail.com>
2025-08-25 11:11:38 +0530

    jesse and cookies is uploaded

---
+++ Hackerrank/week4/Jesse and Cookies.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import heapq
+#
+# Complete the 'cookies' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY A
+#
+
+def cookies(k, A):
+    # Write your code here
+    heapq.heapify(A) 
+    ops=0
+    
+    while len(A)>1 and A[0]<k:
+        l=heapq.heappop(A) 
+        sl=heapq.heappop(A) 
+        cookie=l+2*sl
+        heapq.heappush(A,cookie)
+        ops+=1
+    return ops if A[0]>=k else -1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    A = list(map(int, input().rstrip().split()))
+
+    result = cookies(k, A)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
