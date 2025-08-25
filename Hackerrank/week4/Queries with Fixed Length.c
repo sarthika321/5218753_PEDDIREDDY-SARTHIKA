5345fa49fa17294e14577b924cf0b021b329edc8
sarthika321 <psarthika1@gmail.com>
2025-08-25 11:13:59 +0530

    queries with fixed length is uploaded

---
+++ Hackerrank/week4/Queries with Fixed Length.c
@@ -0,0 +1,261 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY arr
+ *  2. INTEGER_ARRAY queries
+ */
+
+/*
+ * To return the integer array from the function, you should:
+ *     - Store the size of the array to be returned in the result_count variable
+ *     - Allocate the array statically or dynamically
+ *
+ * For example,
+ * int* return_integer_array_using_static_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     static int a[5] = {1, 2, 3, 4, 5};
+ *
+ *     return a;
+ * }
+ *
+ * int* return_integer_array_using_dynamic_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     int *a = malloc(5 * sizeof(int));
+ *
+ *     for (int i = 0; i < 5; i++) {
+ *         *(a + i) = i + 1;
+ *     }
+ *
+ *     return a;
+ * }
+ *
+ */
+int* solve(int arr_count, int* arr, int queries_count, int* queries, int* result_count) {
+    int n=arr_count;
+    int *l=malloc(n*sizeof(int));
+    int *r=malloc(n*sizeof(int));
+    
+    int *stack=malloc(n*sizeof(int));
+    int t=-1;
+    for(int k=0;k<n;k++){
+        while(t>=0 && arr[stack[t]]<=arr[k]) t--;
+        l[k]=(t>=0?stack[t]:-1);
+        stack[++t]=k;
+    }
+    t=-1;
+    for(int k=n-1;k>=0;k--){
+        while(t>=0 && arr[stack[t]]<=arr[k]) t--;
+        r[k]=(t>=0?stack[t]:n);
+        stack[++t]=k;
+    }
+    int *res=malloc((n+1) * sizeof(int));
+    for(int k=0;k<=n;k++) res[k]=INT_MAX;
+    
+    for(int k=0;k<n;k++){
+        int len=r[k]-l[k]-1;
+        if(arr[k]<res[len]) res[len]=arr[k];
+    }
+    for(int k=n-1;k>=1;k--){
+        if(res[k]>res[k+1]) res[k]=res[k+1];
+    }
+    
+    int *a=malloc(queries_count * sizeof(int));
+    for(int k=0;k<queries_count;k++){
+        a[k]=res[queries[k]];
+    }
+    *result_count=queries_count;
+    
+    free(l);
+    free(r);
+    free(stack);
+    free(res);
+    return a;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char** first_multiple_input = split_string(rtrim(readline()));
+
+    int n = parse_int(*(first_multiple_input + 0));
+
+    int q = parse_int(*(first_multiple_input + 1));
+
+    char** arr_temp = split_string(rtrim(readline()));
+
+    int* arr = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = parse_int(*(arr_temp + i));
+
+        *(arr + i) = arr_item;
+    }
+
+    int* queries = malloc(q * sizeof(int));
+
+    for (int i = 0; i < q; i++) {
+        int queries_item = parse_int(ltrim(rtrim(readline())));
+
+        *(queries + i) = queries_item;
+    }
+
+    int result_count;
+    int* result = solve(n, arr, q, queries, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        fprintf(fptr, "%d", *(result + i));
+
+        if (i != result_count - 1) {
+            fprintf(fptr, "\n");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
